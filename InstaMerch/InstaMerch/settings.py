e0bd4143144eb20854cca535581fcfcd3f34139f
SaratAngajalaoffl <saratangajala@gmail.com>
2020-10-14 22:47:02 +0530

    Basic Setup for postgresql

--- InstaMerch/InstaMerch/settings.py
+++ InstaMerch/InstaMerch/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'API',
+    'WEB',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,11 @@ WSGI_APPLICATION = 'InstaMerch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instamerch',
+        'USER': 'postgres',
+        'PASSWORD': 'chandrasar',
+        'HOST': 'localhost'
     }
 }
 
